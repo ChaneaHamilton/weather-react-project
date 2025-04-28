8b66343816d222cb6cbc83887874d462c6381025
ChaneaHamilton <chaneahamilton@gmail.com>
2025-04-28 16:29:34 -0600

    updated links

--- src/Weather.js
+++ src/Weather.js
@@ -1,51 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "New York",
-    temperature: 19,
-    date: "Tuesday 10:00",
-    description: "Cloudy",
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-    humidity: 80,
-    wind: 10,
-  };
-  return (
-    <div className="Weather">
-      <div className="weather-app">
-        <header>
-          <form>
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              required
-              className="search-input"
-            />
-            <input type="submit" value="Search" class="search-button" />
-          </form>
-        </header>
-        <main>
-          <div className="current-weather">
-            <div>
-              <h1 className="current-city">{weatherData.city}</h1>
-              <p className="current-details">
-                {weatherData.description}
-                <br />
-                Humidity: <strong>{weatherData.humidity}%</strong>, Wind:{" "}
-                <strong>{weatherData.wind}km/h</strong>
-              </p>
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "eac360db5fc86ft86450f3693e73o43f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <a
+          href="https://www.shecodes.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/images/logo.png" className="logo" alt="SheCodes Logo" />
+        </a>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9 ">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control search-input"
+                onChange={handleCityChange}
+              />
             </div>
-            <div className="current-temperature">
-              <span className="current-temperature-icon">☀️</span>
-              <span className="current-temperature-value">
-                {weatherData.temperature}
-              </span>
-              <span className="current-temperature-unit">°c</span>
+            <div className="col-3 p-0">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </main>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast
+          coordinates={weatherData.coordinates}
+          city={weatherData.city}
+        />
+        <footer>
+          This project was coded by Chanea Hamilton
+          <a
+            href="https://github.com/ChaneaHamilton"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SheCodes
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/ChaneaHamilton/weather-react-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weather-react-project-chanea.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
